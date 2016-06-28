fcac85bf62bf950a88aa95e5266fa13b81f6194d
Jake <jhj0072000@gmail.com>
2016-06-28 15:30:42 -0600

    major changes

--- public/javascripts/custom.js
+++ public/javascripts/custom.js
@@ -1,54 +1,54 @@
-jQuery(function($){
+  jQuery(function($){
 
-	/* ----------------------------------------------------------- */
-	/*  LOADING 
-	/* ----------------------------------------------------------- */
-	jQuery(window).load(function() { // makes sure the whole site is loaded
-	  jQuery('.clock').fadeOut(); // will first fade out the loading animation
-	  jQuery('#preloader').delay(100).fadeOut('slow'); // will fade out the white DIV that covers the website.
-	  jQuery('body').delay(100).css({'overflow':'visible'});
-	});
+  	/* ----------------------------------------------------------- */
+  	/*  LOADING 
+  	/* ----------------------------------------------------------- */
+  	jQuery(window).load(function() { // makes sure the whole site is loaded
+  	  jQuery('.clock').fadeOut(); // will first fade out the loading animation
+  	  jQuery('#preloader').delay(100).fadeOut('slow'); // will fade out the white DIV that covers the website.
+  	  jQuery('body').delay(100).css({'overflow':'visible'});
+  	});
 
-	/* ----------------------------------------------------------- */
-	/*  SCROLL TOP BUTTON
-	/* ----------------------------------------------------------- */
-	jQuery(window).scroll(function(){
-    if ($(this).scrollTop() > 300) {
-      $('.scrollToTop').fadeIn();
-    } else {
-      $('.scrollToTop').fadeOut();
-    }
-  });
-  //Click event to scroll to top
-  jQuery('.scrollToTop').click(function(){
-    $('html, body').animate({scrollTop : 0},800);
-    return false;
-  });
-
-  /* ----------------------------------------------------------- */
-  /*  NAVIGATION
-  /* ----------------------------------------------------------- */
-  $(document).ready(function() {
-    $('[data-toggle=collapse-side]').click(function(e) {
-      $('.side-collapse').toggleClass('open');
+  	/* ----------------------------------------------------------- */
+  	/*  SCROLL TOP BUTTON
+  	/* ----------------------------------------------------------- */
+  	jQuery(window).scroll(function(){
+      if ($(this).scrollTop() > 300) {
+        $('.scrollToTop').fadeIn();
+      } else {
+        $('.scrollToTop').fadeOut();
+      }
     });
-  });
-  $('.navbar-toggle').click(function () {
-    var text = $(this).text();
-    if($('#toggle-selector').hasClass("open")){
-      $(this).text(text.replace('MORE +', 'LESS -'));
-      $('body').css("overflow", "hidden");
-    } else {
-      $(this).text(text.replace('LESS -', 'MORE +')); 
-      $('body').css("overflow", "visible");
-    }
-  });
-  
-  // MENU HANDLER
-  // var url = window.location;
-  // $('ul.nav a').filter(function() {
-  //     return this.href == url;
-  // }).parent().addClass('active');
+    //Click event to scroll to top
+    jQuery('.scrollToTop').click(function(){
+      $('html, body').animate({scrollTop : 0},800);
+      return false;
+    });
+
+    /* ----------------------------------------------------------- */
+    /*  NAVIGATION
+    /* ----------------------------------------------------------- */
+    $(document).ready(function() {
+      $('[data-toggle=collapse-side]').click(function(e) {
+        $('.side-collapse').toggleClass('open');
+      });
+    });
+    $('.navbar-toggle').click(function () {
+      var text = $(this).text();
+      if($('#toggle-selector').hasClass("open")){
+        $(this).text(text.replace('MORE +', 'LESS -'));
+        $('body').css("overflow", "hidden");
+      } else {
+        $(this).text(text.replace('LESS -', 'MORE +')); 
+        $('body').css("overflow", "visible");
+      }
+    });
+    
+    // MENU HANDLER
+    // var url = window.location;
+    // $('ul.nav a').filter(function() {
+    //     return this.href == url;
+    // }).parent().addClass('active');
 
   var first = $(location).attr('pathname');
   first.indexOf(1);
@@ -66,28 +66,34 @@ jQuery(function($){
   } else if (firstURL == "connect") {
     $("#connectnav").addClass("active");
   }
-  
-  // STICKY
-  $(document).ready(function(){
-    $("#sticker").sticky({topSpacing:0});
+
+    // STICKY
+    $(document).ready(function(){
+      $("#sticker").sticky({topSpacing:0});
+    });
+
+    // PAGE TABS
+    $.fn.responsiveTabs = function() {
+      this.addClass('responsive-tabs');
+      this.append($('<span class="glyphicon glyphicon-triangle-bottom"></span>'));
+      this.append($('<span class="glyphicon glyphicon-triangle-top"></span>'));
+
+      this.on('click', 'li.active > a, span.glyphicon', function() {
+        this.toggleClass('open');
+      }.bind(this));
+
+      this.on('click', 'li:not(.active) > a', function() {
+        this.removeClass('open');
+      }.bind(this));
+    };
+
+    $('.nav.nav-tabs').responsiveTabs();
+
+
+    /* ----------------------------------------------------------- */
+    /*  PAGES
+    /* ----------------------------------------------------------- */
+
+
+
   });
-
-  // PAGE TABS
-  $.fn.responsiveTabs = function() {
-    this.addClass('responsive-tabs');
-    this.append($('<span class="glyphicon glyphicon-triangle-bottom"></span>'));
-    this.append($('<span class="glyphicon glyphicon-triangle-top"></span>'));
-
-    this.on('click', 'li.active > a, span.glyphicon', function() {
-      this.toggleClass('open');
-    }.bind(this));
-
-    this.on('click', 'li:not(.active) > a', function() {
-      this.removeClass('open');
-    }.bind(this));
-  };
-
-  $('.nav.nav-tabs').responsiveTabs();
-
-
-});
