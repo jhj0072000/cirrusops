3b29cadea31ce4e90b6ecaadd87742571ee0f930
Jake <jhj0072000@gmail.com>
2016-07-05 11:11:01 -0600

    litle changes here and there

--- public/javascripts/custom.js
+++ public/javascripts/custom.js
@@ -114,19 +114,19 @@
     /*  PAGES
     /* ----------------------------------------------------------- */
     $('a[href*=#]:not([href=#])').click(function() {
-      if (location.pathname.replace(/^\//,'') == this.pathname.replace(/^\//,'') 
-        || location.hostname == this.hostname) {
+        if (location.pathname.replace(/^\//,'') == this.pathname.replace(/^\//,'') 
+          || location.hostname == this.hostname) {
 
-        var target = $(this.hash);
-      target = target.length ? target : $('[name=' + this.hash.slice(1) +']');
-      if (target.length) {
-       $('html,body').animate({
-        scrollTop: target.offset().top - 140
-      }, 1000);
-       return false;
+          var target = $(this.hash);
+        target = target.length ? target : $('[name=' + this.hash.slice(1) +']');
+        if (target.length) {
+         $('html,body').animate({
+          scrollTop: target.offset().top - 140
+        }, 1000);
+         return false;
+       }
      }
-   }
-  });
+    });
 
 
   });
