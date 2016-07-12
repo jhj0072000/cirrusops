5d91467b1d16dc129471b5710784c8834cd8fecf
Jake <jhj0072000@gmail.com>
2016-07-12 09:38:03 -0600

    angular elements

--- public/javascripts/controller.js
+++ public/javascripts/controller.js
@@ -1,24 +1,117 @@
 var cirrusOps = angular.module('cirrusOps',[]);
 
-cirrusOps.controller('TestController', ['$scope', function($scope) {
-	person1 = {
-		name: 'tim',
-		email: 'tim@gmail.com',
-		number: '1'
+/* ----------------------------------------------------------- */
+/*  SUB MENU HANDLER FOR DETAIL PAGES
+/* ----------------------------------------------------------- */
+cirrusOps.controller('ScrollController', ['$scope', function($scope) {
+	$(document).ready(function () {
+		$(document).on("scroll", onScroll);
+		$('a[href^="#"]').on('click', function (e) {
+			e.preventDefault();
+			$(document).off("scroll");
+			$('a').each(function () {
+				$(this).removeClass('active');
+			})
+			$(this).addClass('active');
+			var target = this.hash,
+			menu = target;
+			$target = $(target);
+			$('html, body').stop().animate({
+				'scrollTop': $target.offset().top - 180
+			}, 500, 'swing', function () {
+				window.location.hash = target;
+				$(document).on("scroll", onScroll);
+			});
+		});
+	});
+	function onScroll(event){
+		var scrollPos = $(document).scrollTop() + 180;
+		$('#solution-menu a').each(function () {
+			var currLink = $(this);
+			var refElement = $(currLink.attr("href"));
+			if (refElement.position().top <= scrollPos && refElement.position().top + refElement.height() > scrollPos) {
+				$('#solution-menu ul li a').removeClass("active");
+				currLink.addClass("active");
+			}
+			else{
+				currLink.removeClass("active");
+			}
+		});
 	}
-	person2 = {
-		name: 'emily',
-		email: 'emily@gmail.com',
-		number: '2'
-	}
-	person3 = {
-		name: 'john',
-		email: 'john@gmail.com',
-		number: '3'
-	}
+}]);
 
-	var contactList = [person1, person2, person3];
+/* ----------------------------------------------------------- */
+/*  OWL CAROUSEL
+/* ----------------------------------------------------------- */
+cirrusOps.controller('OwlController', ['$scope', function($scope) {
+	$(document).ready(function() {
 
-	$scope.contactList = contactList;
+		var sync1 = $("#sync1");
+		var sync2 = $("#sync2");
 
+		sync1.owlCarousel({
+			singleItem : true,
+			slideSpeed : 1000,
+			pagination: true,
+			paginationSpeed: 400,
+			slideSpeed: 400,
+			autoPlay: 7000,
+			afterAction : syncPosition,
+			responsiveRefreshRate : 300,
+		});
+
+		sync2.owlCarousel({
+			touchDrag: false,
+			mouseDrag: false,
+			items: 4,
+			pagination:false,
+			responsiveRefreshRate : 100,
+			afterInit : function(el){
+				el.find(".owl-item").eq(0).addClass("synced");
+			}
+		});
+
+		function syncPosition(el){
+			var current = this.currentItem;
+			$("#sync2")
+			.find(".owl-item")
+			.removeClass("synced")
+			.eq(current)
+			.addClass("synced")
+			if($("#sync2").data("owlCarousel") !== undefined){
+				center(current)
+			}
+		}
+
+		$("#sync2").on("click", ".owl-item", function(e){
+			e.preventDefault();
+			var number = $(this).data("owlItem");
+			sync1.trigger("owl.goTo",number);
+		});
+
+		function center(number){
+			var sync2visible = sync2.data("owlCarousel").owl.visibleItems;
+			var num = number;
+			var found = false;
+			for(var i in sync2visible){
+				if(num === sync2visible[i]){
+					var found = true;
+				}
+			}
+			if(found===false){
+				if(num>sync2visible[sync2visible.length-1]){
+					sync2.trigger("owl.goTo", num - sync2visible.length+1)
+				}else{
+					if(num - 1 === -1){
+						num = 0;
+					}
+					sync2.trigger("owl.goTo", num);
+				}
+			} else if(num === sync2visible[sync2visible.length-1]){
+				sync2.trigger("owl.goTo", sync2visible[1])
+			} else if(num === sync2visible[0]){
+				sync2.trigger("owl.goTo", num-1)
+			}
+		}
+	});
 }]);
--- public/javascripts/custom.js
+++ public/javascripts/custom.js
@@ -23,82 +23,6 @@ $('.scrollToTop').click(function(){
   return false;
 });
 
-/* ----------------------------------------------------------- */
-/*  OWL CAROUSEL
-/* ----------------------------------------------------------- */
-$(document).ready(function() {
- 
-  var sync1 = $("#sync1");
-  var sync2 = $("#sync2");
- 
-  sync1.owlCarousel({
-    singleItem : true,
-    slideSpeed : 1000,
-    pagination: true,
-    paginationSpeed: 400,
-    slideSpeed: 400,
-    autoPlay: 7000,
-    afterAction : syncPosition,
-    responsiveRefreshRate : 300,
-  });
- 
-  sync2.owlCarousel({
-    touchDrag: false,
-    mouseDrag: false,
-    items: 4,
-    pagination:false,
-    responsiveRefreshRate : 100,
-    afterInit : function(el){
-      el.find(".owl-item").eq(0).addClass("synced");
-    }
-  });
- 
-  function syncPosition(el){
-    var current = this.currentItem;
-    $("#sync2")
-      .find(".owl-item")
-      .removeClass("synced")
-      .eq(current)
-      .addClass("synced")
-    if($("#sync2").data("owlCarousel") !== undefined){
-      center(current)
-    }
-  }
- 
-  $("#sync2").on("click", ".owl-item", function(e){
-    e.preventDefault();
-    var number = $(this).data("owlItem");
-    sync1.trigger("owl.goTo",number);
-  });
- 
-  function center(number){
-    var sync2visible = sync2.data("owlCarousel").owl.visibleItems;
-    var num = number;
-    var found = false;
-    for(var i in sync2visible){
-      if(num === sync2visible[i]){
-        var found = true;
-      }
-    }
- 
-    if(found===false){
-      if(num>sync2visible[sync2visible.length-1]){
-        sync2.trigger("owl.goTo", num - sync2visible.length+1)
-      }else{
-        if(num - 1 === -1){
-          num = 0;
-        }
-        sync2.trigger("owl.goTo", num);
-      }
-    } else if(num === sync2visible[sync2visible.length-1]){
-      sync2.trigger("owl.goTo", sync2visible[1])
-    } else if(num === sync2visible[0]){
-      sync2.trigger("owl.goTo", num-1)
-    }
-    
-  }
- 
-});
 
 /* ----------------------------------------------------------- */
 /*  NAVIGATION
@@ -143,46 +67,6 @@ if (secondURL == "quickbase") {
 } else if (secondURL == "spreadsheet") {
   $("#spreadsheetnav").addClass("active");
 }
-// SUB MENU HANDLER FOR DETAIL PAGES
-$(document).ready(function () {
-    $(document).on("scroll", onScroll);
-    $('a[href^="#"]').on('click', function (e) {
-        e.preventDefault();
-        $(document).off("scroll");
-        $('a').each(function () {
-            $(this).removeClass('active');
-        })
-        $(this).addClass('active');
-        var target = this.hash,
-            menu = target;
-        $target = $(target);
-        $('html, body').stop().animate({
-            'scrollTop': $target.offset().top - 180
-        }, 500, 'swing', function () {
-            window.location.hash = target;
-            $(document).on("scroll", onScroll);
-        });
-    });
-});
-
-function onScroll(event){
-    var scrollPos = $(document).scrollTop() + 180;
-    $('#solution-menu a').each(function () {
-        var currLink = $(this);
-        var refElement = $(currLink.attr("href"));
-        if (refElement.position().top <= scrollPos && refElement.position().top + refElement.height() > scrollPos) {
-            $('#solution-menu ul li a').removeClass("active");
-            currLink.addClass("active");
-        }
-        else{
-            currLink.removeClass("active");
-        }
-    });
-}
-
-
-
-
 
 // PAGE TABS
 $.fn.responsiveTabs = function() {
