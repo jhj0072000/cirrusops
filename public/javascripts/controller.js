5fd4c76b85224bc98cab29ce064fca9e77f7c37a
Jake <jhj0072000@gmail.com>
2016-07-13 13:33:53 -0600

    solar edits

--- public/javascripts/controller.js
+++ public/javascripts/controller.js
@@ -44,40 +44,18 @@ cirrusOps.controller('ScrollController', ['$scope', function($scope) {
 /*  SUB MENU HANDLER FOR LANDING PAGE
 /* ----------------------------------------------------------- */
 cirrusOps.controller('ScrollController2', ['$scope', function($scope) {
-	$(document).ready(function () {
-		$(document).on("scroll", onScroll);
-		$('a[href^="#"]').on('click', function (e) {
-			e.preventDefault();
-			$(document).off("scroll");
-			$('a').each(function () {
-				$(this).removeClass('active');
-			})
-			$(this).addClass('active');
-			var target = this.hash,
-			menu = target;
-			$target = $(target);
-			$('html, body').stop().animate({
-				'scrollTop': $target.offset().top - 84
-			}, 500, 'swing', function () {
-				window.location.hash = target;
-				$(document).on("scroll", onScroll);
-			});
-		});
-	});
-	function onScroll(event){
-		var scrollPos = $(document).scrollTop() + 84;
-		$('#solution-menu a').each(function () {
-			var currLink = $(this);
-			var refElement = $(currLink.attr("href"));
-			if (refElement.position().top <= scrollPos && refElement.position().top + refElement.height() > scrollPos) {
-				$('#solution-menu ul li a').removeClass("active");
-				currLink.addClass("active");
-			}
-			else{
-				currLink.removeClass("active");
-			}
-		});
-	}
+	$('a[href*="#"]:not([href="#"])').click(function() {
+    if (location.pathname.replace(/^\//,'') == this.pathname.replace(/^\//,'') && location.hostname == this.hostname) {
+      var target = $(this.hash);
+      target = target.length ? target : $('[name=' + this.hash.slice(1) +']');
+      if (target.length) {
+        $('html, body').animate({
+          scrollTop: target.offset().top - 80
+        }, 1000);
+        return false;
+      }
+    }
+  });
 }]);
 
 /* ----------------------------------------------------------- */
